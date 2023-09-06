ccae6ca0df24cac14fc55d213fde023eb24d79b7
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-06 21:35:48 +0300

    Ivanchenko [ADD]: added full solution of Task01

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,20 @@
-﻿// Your code here!
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите вашу фамилию: ");
+        string secondname = Console.ReadLine();
+
+        Console.Write("Введите ваше имя: ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("Введите ваше отчество: ");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine("Фамилия: " + secondname);
+        Console.WriteLine("Имя: " + firstname);
+        Console.WriteLine("Отчество: " + surname);
+    }
+}
