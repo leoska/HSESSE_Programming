c756599d9603317ec0ca306261e84a38ed8f1ba8
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-07 13:03:57 +0300

    Ivanchenko [ADD]: add full solution of task 3

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,14 @@
-﻿// Your code here!
+﻿using System.Reflection.Metadata;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя пользователя: ");
+        string username = Console.ReadLine();
+
+
+        Console.WriteLine("Здравствуйте, " + username + "!");
+    }
+}
