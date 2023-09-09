bb6becf449b1b9c951ce453bb612ce7e8e309a71
nikita <82255886+businessmemq@users.noreply.github.com>
2023-09-09 22:14:28 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,10 @@
-﻿// Your code here!
+using System;
+class Program
+{
+    static void Main()
+    {
+        double u = double.Parse(Console.ReadLine()), r = double.Parse(Console.ReadLine());
+        double i = u / r, p = u * u / r;
+        Console.WriteLine(i + " " + p);
+    }
+}
