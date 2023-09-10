cbc5a2bbb2e5e3666560841ef5ff3a0dad5c7a98
seny03 <arseniysemkin3012@gmail.com>
2023-09-10 12:29:19 +0300

    Task03 solved

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,13 @@
-﻿// Your code here!
+﻿namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}!");
+        }
+    }
+}
