c07a6c0870ce3f80b3a5925767e97abc0c2c7a27
seny03 <arseniysemkin3012@gmail.com>
2023-09-10 12:24:28 +0300

    Task02 solved

--- Seminars/Seminar01/Self/Task02/Program.cs
+++ Seminars/Seminar01/Self/Task02/Program.cs
@@ -1,1 +1,14 @@
-﻿// Your code here!
+﻿namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? lastName = Console.ReadLine();
+            string? firstName = Console.ReadLine();
+            string? secondName = Console.ReadLine();
+
+            Console.WriteLine($"Фамилия Имя Отчетво: {lastName} {firstName} {secondName}");
+        }
+    }
+}
