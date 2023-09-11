c603bb877dc17577f04f84d211e533d1b9f3e12f
seny03 <arseniysemkin3012@gmail.com>
2023-09-11 06:59:23 +0300

    Task04 solved

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,35 @@
-﻿// Your code here!
+﻿using System.Drawing;
+
+namespace Task04
+{
+    class Program
+    {
+        public static System.ConsoleColor getRandomColor()
+        {
+            ConsoleColor[] colors = (ConsoleColor[]) ConsoleColor.GetValues(typeof(ConsoleColor));
+            Random random = new Random();
+            return colors[random.Next(0, colors.Length-1)];
+        }
+
+        public static void beutifulWriteLine(string str)
+        {
+            Console.BackgroundColor = getRandomColor();
+            Console.ForegroundColor = getRandomColor();
+            Console.WriteLine(str); 
+        }
+
+        public static void Main()
+        {
+            string? lastName = Console.ReadLine();
+            string? firstName = Console.ReadLine();
+            string? secondName = Console.ReadLine();
+
+            beutifulWriteLine($"Фамилия: {lastName}");
+            beutifulWriteLine($"Имя: {firstName}");
+            beutifulWriteLine($"Отчество: {secondName}");
+
+            // Restore the original console colors.
+            Console.ResetColor();
+        }
+    }
+}
