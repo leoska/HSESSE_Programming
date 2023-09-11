37d36a17a1c59c4010e84ca7bb6cd56f893936f0
seny03 <arseniysemkin3012@gmail.com>
2023-09-11 10:11:06 +0300

    Task06 solved

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину одного катета: ");
+            double leg1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Введите длину другого катета: ");
+            double leg2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Длина гипотенузы: {Math.Sqrt(leg1*leg1 + leg2*leg2)}");
+        }
+    }
+}
