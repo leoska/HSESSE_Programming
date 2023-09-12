6fce12995c2e50314199cdedaf4f5fa24632eaa3
Эрик Яковлев <erikakovlev@MacBook-Air-Emil.local>
2023-09-13 00:34:55 +0300

    Task05

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введите напряжение: ");
+        double u=Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите сопротивление ");
+        double r=Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Сила тока = "+(u/r));
+        Console.WriteLine("Потребляемая мощность электрической цепи: "+(u*u/r));
+    }
+}
