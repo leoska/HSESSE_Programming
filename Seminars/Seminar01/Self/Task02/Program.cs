49ae86cf54196dd400bd6e1c9f40e849228e85ad
ekhossejn <ekhossejn@gmail.com>
2023-09-12 15:04:56 +0300

    moo

--- Seminars/Seminar01/Self/Task02/Program.cs
+++ Seminars/Seminar01/Self/Task02/Program.cs
@@ -1,1 +1,14 @@
-﻿// Your code here!
+﻿namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? lastname = Console.ReadLine();
+            string? name = Console.ReadLine();
+            string? secondname = Console.ReadLine();
+
+            Console.WriteLine($"{lastname} {name} {secondname}");
+        }
+    }
+}
