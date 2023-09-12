464193f51e3e87414185474ead22290926c746db
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-12 16:59:42 +0300

    Ivanchenko [ADD]: add full solution of task 6

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+	const ConsoleColor mainTextColor = ConsoleColor.White;
+	const ConsoleColor inputTextColor = ConsoleColor.DarkRed;
+    const ConsoleColor formulaTextColor = ConsoleColor.Cyan;
+    const ConsoleColor answerTextColor = ConsoleColor.Yellow;
+
+	static void Main(string[] args)
+	{
+        Console.Clear();
+
+		Console.ForegroundColor = mainTextColor;
+		Console.Write("Введите длинну первого катета a = ");
+		Console.ForegroundColor = inputTextColor;
+		double a = double.Parse(Console.ReadLine());
+
+		Console.ForegroundColor = mainTextColor;
+		Console.Write("Введите длинну второго катета b = ");
+		Console.ForegroundColor = inputTextColor;
+		double b = double.Parse(Console.ReadLine());
+
+        Console.ForegroundColor = mainTextColor;
+        Console.Write("В п/у треугольнике с катетами a и b гипотенуза с равна ");
+        Console.ForegroundColor = formulaTextColor;
+		Console.Write($"sqrt(a * a + b * b) = sqrt({a} * {a} + {b} * {b}) = ");
+        Console.ForegroundColor = answerTextColor;
+        Console.WriteLine(Math.Sqrt(a * a + b * b));
+ 
+		Console.ResetColor();
+	}
+}
