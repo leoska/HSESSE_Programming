bf6c2803659752f11aef5872576b61701dd3a9e9
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-12 16:31:26 +0300

    Ivanchenko [ADD]: add full solution of task 4

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,44 @@
-﻿// Your code here!
+﻿using System;
+
+class Program
+{
+	const ConsoleColor mainBackgroundColor = ConsoleColor.DarkGray;
+	const ConsoleColor outputBackgroundColor = ConsoleColor.Black;
+	const ConsoleColor mainTextColor = ConsoleColor.DarkBlue;
+	const ConsoleColor inputTextColor = ConsoleColor.Red;
+
+	static void Main(string[] args)
+	{
+		Console.BackgroundColor = mainBackgroundColor;
+		Console.Clear();
+
+		Console.ForegroundColor = mainTextColor;
+		Console.Write("Введите вашу фамилию: ");
+		Console.ForegroundColor = inputTextColor;
+		string secondname = Console.ReadLine();
+
+		Console.ForegroundColor = mainTextColor;
+		Console.Write("Введите ваше имя: ");
+		Console.ForegroundColor = inputTextColor;
+		string firstname = Console.ReadLine();
+
+		Console.ForegroundColor = mainTextColor;
+		Console.Write("Введите ваше отчество: ");
+		Console.ForegroundColor = inputTextColor;
+		string surname = Console.ReadLine();
+
+
+		Console.BackgroundColor = outputBackgroundColor;
+		
+		string name = secondname + " " + firstname + " " + surname;
+
+		for (int i = 0; i < name.Length; i++) {
+			Console.ForegroundColor = (ConsoleColor)(10 + i % 6);
+			Console.Write(name[i]);
+		}
+
+		Console.WriteLine();
+
+		Console.ResetColor();
+	}
+}
