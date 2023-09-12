827c5a93fc0794c6ad45295d34ea7a3b902fdcff
Влад Домрачев <vadomrachev@edu.hse.ru>
2023-09-12 22:44:32 +0300

    Домрачев 6

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HSE_1_Seminar1_Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task 6
+            Console.WriteLine("Для подсчета, пожалуйста, введите значения");
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt(a * a + b * b));
+        }
+    }
+}
