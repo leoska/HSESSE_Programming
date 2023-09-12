856dcb1fff40a96ddd799f465f9bd3e044394b38
Влад Домрачев <vadomrachev@edu.hse.ru>
2023-09-12 22:41:54 +0300

    Домрачев 5

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,18 @@
-﻿// Your code here!
+﻿using System;
+
+namespace HSE_1_Seminar1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 5
+            Console.WriteLine("Для подсчета, пожалуйста, введите значения");
+            Console.Write("U = ");
+            double U = Convert.ToDouble(Console.ReadLine());
+            Console.Write("I = ");
+            double I = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(U * U / I);
+        }
+    }
+}
