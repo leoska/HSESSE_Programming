0c74370c75323710b4e9532950ea0588b064057a
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-12 17:11:32 +0300

    Ivanchenko [EDIT]: add comments to solution of task 3

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -4,11 +4,12 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        // Запрос имени пользователя и его сохрание
         Console.Write("Введите ваше имя пользователя: ");
         string username = Console.ReadLine();
 
-
+        // Привествие пользователя
         Console.WriteLine("Здравствуйте, " + username + "!");
     }
 }
