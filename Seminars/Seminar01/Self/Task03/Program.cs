e659e5e50fb53bb2f013177e9306013044619d8c
qoxrdw <egorskorik398@gmail.com>
2023-09-13 02:48:42 +0300

    completed tasks

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,17 @@
-﻿// Your code here!
+﻿// Скорик Егор 239-2
+
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите имя пользователя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.WriteLine($"Здравствуйте, {name}!");
+
+    }
+}
--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Скорик Егор 239-2
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение первого катета: ");
+        double.TryParse(Console.ReadLine().Replace('.', ','), out double k1);
+        Console.WriteLine();
+
+        Console.Write("Введите значение второго катета: ");
+        double.TryParse(Console.ReadLine().Replace('.', ','), out double k2);
+        Console.WriteLine();
+
+        if (k1 != 0 && k2 != 0)
+        {
+            Console.WriteLine($"Гипотенуза равна: {Math.Pow(k1 * k1 + k2 * k2, 0.5)}");
+        }
+        else
+        {
+            Console.WriteLine("Катеты не могут быть равны нулю");
+        }
+
+
+
+    }
+}
