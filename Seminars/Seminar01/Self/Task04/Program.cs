5539f3335ba2a83cdbf6e7878d89ba01af3a63ee
Влад Домрачев <vadomrachev@edu.hse.ru>
2023-09-12 22:40:05 +0300

    Домрачев 4

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -10,8 +10,6 @@ namespace HSE_1_Seminar1_4
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Magenta;
             Console.Clear();
-            Console.WriteLine("-Hi, Ken!");
-            Console.WriteLine("-Hi, Barbie!");
         }
     }
 }
