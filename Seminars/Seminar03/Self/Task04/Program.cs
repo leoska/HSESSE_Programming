0ea8deaa492c0c8a2ea2e7225c240f5f66e10b50
zemld <zeml.day@gmail.com>
2023-09-17 22:29:23 +0300

    Seminar03 Self

---
+++ Seminars/Seminar03/Self/Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task03;
+class Program
+{
+    static void Main()
+    {
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        double f;
+        if (x < y)
+        {
+            f = Math.Sin(x) + Math.Cos(y) * Math.Cos(y);
+        }
+        else if (x == y)
+        {
+            f = Math.Log(Math.Abs(x));
+        }
+        else
+        {
+            f = Math.Sin(x * x) + Math.Cos(y);
+        }
+        Console.WriteLine(f);
+    }
+}
---
+++ Seminars/Seminar03/Self/Task05/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task05;
+class Program
+{
+    static void Main()
+    {
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Уравнение: {a}*x^2 + {b}*x + {c} = 0");
+
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                {
+                    Console.WriteLine("Решением уравнения является любое действительное число");
+                }
+                else
+                {
+                    Console.WriteLine("Решений нет");
+                }
+            }
+            else
+            {
+                double x = (-c) / b;
+                Console.WriteLine($"Уравнение имеет единственный корень х = {Math.Round(x, 3)}");
+            }
+        }
+        else
+        {
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+            {
+                Console.WriteLine("Уравнение не имеет решений");
+            }
+            else if (discriminant > 0)
+            {
+                double x1 = (-b + Math.Pow(discriminant, 0.5)) / (2 * a);
+                double x2 = (-b - Math.Pow(discriminant, 0.5)) / (2 * a);
+
+                Console.WriteLine($"Уравнение имеет корни x1 = {Math.Round(x1, 3)} и х2 = {Math.Round(x2, 3)}");
+            }
+            else
+            {
+                double x = (-b) / (2 * a);
+
+                Console.WriteLine($"Уравнение имеет два совпадающий корня, равных {Math.Round(x, 3)}");
+            }
+        }
+    }
+}
