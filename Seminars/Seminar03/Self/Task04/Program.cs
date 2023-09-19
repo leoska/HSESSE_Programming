ca4ac615dcc1a144efde8e432f7bc517df6a0b71
nikita <82255886+businessmemq@users.noreply.github.com>
2023-09-19 22:55:24 +0300

    Add files via upload

---
+++ Seminars/Seminar03/Self/Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+        double f = 0.0;
+        if (x < y)
+        {
+            f = Math.Sin(x) + Math.Cos(y) * Math.Cos(y);
+        }
+        else if (x == y)
+        {
+            f = Math.Log(Math.Abs(x));
+        }
+        else
+        {
+            f = Math.Sin(x * x) + Math.Cos(y);
+        }
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        Console.WriteLine(f);
+    }
+}
