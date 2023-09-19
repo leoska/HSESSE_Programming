a118dab029c222ef7c61ad0dd668036067dc8362
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-20 01:01:43 +0300

    Ivanchenko: [ADD] add full solution of Task04

---
+++ Seminars/Seminar02/Self/Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HellowWorld
+{
+class Program
+	{
+		static void Main()
+		{
+            // Создание и считывание из консоли символьной переменной
+			char a = char.Parse(Console.ReadLine());
+
+            // Вывод чем является переменная в консоль
+            Console.WriteLine($"{(('0' <= a && a <= '9') ? "Это цифра" : ((('a' <= a && a <= 'z') || ('A' <= a && a <= 'Z')) ? "Это буква" : "Это ни буква, ни цифра"))}");
+        }
+	}
+}
