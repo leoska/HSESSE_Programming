ee4a58285fe6c1e35c04eaed3d755f7e6bee53ca
Vlad <vlddom5@mail.ru>
2023-09-19 16:20:11 +0300

    1

--- Seminars/Seminar03/Self/Task01/Programm.cs
+++ Seminars/Seminar03/Self/Task01/Programm.cs
@@ -1,1 +1,25 @@
-//Test
+using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double l, ans;
+
+            n = int.Parse(Console.ReadLine());
+            if (n <= 2)
+            {
+                Console.WriteLine("Неверный ввод!");
+                return;
+            }
+            l = Double.Parse(Console.ReadLine());
+
+            ans = (n * l * l / (4 * Math.Tan(Math.PI / n)));
+
+            Console.WriteLine(ans);
+        }
+    }
+}
