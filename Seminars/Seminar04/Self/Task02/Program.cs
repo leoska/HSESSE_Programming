7bd7209fcd6b03daf734568d95ab800b0b5ce1ea
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-20 00:05:56 +0300

    Ivanchenko: [ADD] add full solution of Task02

---
+++ Seminars/Seminar04/Self/Task02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Prak3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите название месяца / Enter month name: ");
+
+            int monthNum = Console.ReadLine() switch {
+                "January" or "Январь" => 1,
+                "February" or "Февраль" => 2,
+                "March" or "Март" => 3,
+                "April" or "Апрель" => 4,
+                "May" or "Май" => 5,
+                "June" or "Июнь" => 6,
+                "July" or "Июль" => 7,
+                "August" or "Август" => 8,
+                "September" or "Сентябрь" => 9,
+                "October" or "Октябрь" => 10,
+                "November" or "Ноябрь" => 11,
+                "December" or "Декабрь" => 12,
+                _ => 0
+            };
+
+            if (monthNum != 0)
+            {
+                Console.WriteLine($"Номер месяца / Month number: {monthNum}");
+            } else
+            {
+                Console.WriteLine("В году такой месяц отсутсвует / There is no such month in the year");
+            }
+        }
+    }
+}
