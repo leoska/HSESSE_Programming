5a702f40de4057a9d68d13c8bef14e595f8f1750
Анна Асташкина <adastashkina@edu.hse.ru>
2023-09-19 23:48:40 +0300

    Seminar04 self programms

---
+++ Seminars/Seminar04/Self/Self_programms/task02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+ internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            switch (month)
+            {
+                case "January" or "Январь":
+                    Console.WriteLine("1");
+                    break;
+                case "February" or "Ноябрь":
+                    Console.WriteLine("2");
+                    break;
+                case "March" or "Март":
+                    Console.WriteLine("3");
+                    break;
+                case "April" or "Апрель":
+                    Console.WriteLine("4");
+                    break;
+                case "May" or "Май":
+                    Console.WriteLine("5");
+                    break;
+                case "June" or "Июнь":
+                    Console.WriteLine("6");
+                    break;
+                case "July" or "Июль":
+                    Console.WriteLine("7");
+                    break;
+                case "August" or "Август":
+                    Console.WriteLine("8");
+                    break;
+                case "September" or "Сентябрь":
+                    Console.WriteLine("9");
+                    break;
+                case "October" or "Октябрь":
+                    Console.WriteLine("10");
+                    break;
+                case "November" or "Ноябрь":
+                    Console.WriteLine("11");
+                    break;
+                case "December" or "Декабрь":
+                    Console.WriteLine("12");
+                    break;
+                default:
+                    Console.WriteLine("В году такой год отсутствует.");
+                    break;
+
+
+            }
+            Console.ReadKey();
+        }
+    }
---
+++ Seminars/Seminar04/Self/Self_programms/task04/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string s_in, s_out;
+        s_in = Console.ReadLine();
+        if (s_in.Contains("-"))
+        {
+            int numb = s_in.IndexOf("-");
+            double a, b, c;
+            string srez1 = (s_in.Substring(0, numb)).Trim();  // Срез первого числа
+            int len = s_in.Length;
+            string srez2 = (s_in.Substring(numb+1, len-numb-1)).Trim();  // Срез второго числа
+            a = double.Parse(srez1);
+            b = double.Parse(srez2);
+            c = a - b;
+            Console.WriteLine($"результат вычислений: {c}");
+        }
+        else if (s_in.Contains("+"))
+        {
+            int numb = s_in.IndexOf("+");
+            double a, b, c;
+            string srez1 = (s_in.Substring(0, numb)).Trim();  // Срез первого числа
+            int len = s_in.Length;
+            string srez2 = (s_in.Substring(numb + 1, len - numb - 1)).Trim();  // Срез второго числа
+            a = double.Parse(srez1);
+            b = double.Parse(srez2);
+            c = a + b;
+            Console.WriteLine($"результат вычислений: {c}");
+        }
+        else if (s_in.Contains("*"))
+        {
+            int numb = s_in.IndexOf("*");
+            double a, b, c;
+            string srez1 = (s_in.Substring(0, numb)).Trim();  // Срез первого числа
+            int len = s_in.Length;
+            string srez2 = (s_in.Substring(numb + 1, len - numb - 1)).Trim();  // Срез второго числа
+            a = double.Parse(srez1);
+            b = double.Parse(srez2);
+            c = a * b;
+            Console.WriteLine($"результат вычислений: {c}");
+        }
+        else if (s_in.Contains("/"))
+        {
+            int numb = s_in.IndexOf("/");
+            double a, b, c;
+            string srez1 = (s_in.Substring(0, numb)).Trim();  // Срез первого числа
+            int len = s_in.Length;
+            string srez2 = (s_in.Substring(numb + 1, len - numb - 1)).Trim();  // Срез второго числа
+            a = double.Parse(srez1);
+            b = double.Parse(srez2);
+            c = a / b;
+            Console.WriteLine($"результат вычислений: {c}");
+        }
+        else
+        {
+            Console.WriteLine("Введены некорректные данные");
+        }
+    }
+    }
