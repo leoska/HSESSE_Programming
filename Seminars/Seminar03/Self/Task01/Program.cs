2ed2b857968a52ab150b078c97c29235170f8ca2
nikita <82255886+businessmemq@users.noreply.github.com>
2023-09-19 22:24:28 +0300

    Add files via upload

---
+++ Seminars/Seminar03/Self/Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        double l = Convert.ToDouble(Console.ReadLine());
+        double s = n * l * l / (4 * Math.Tan(Math.PI / n));
+        Console.WriteLine(s);
+    }
+}
