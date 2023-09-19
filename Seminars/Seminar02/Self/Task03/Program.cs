001e9fca1ad941a90aaba93253745a59a901a128
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-20 00:56:06 +0300

    Ivanchenko: [ADD] add full solution of Task03

---
+++ Seminars/Seminar02/Self/Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HellowWorld
+{
+class Program
+	{
+		static void Main()
+		{
+            // Создание и инициализация переменных
+			int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            // Меняем местами значения, чтобы было x <= y <= z
+            int mn = (x < y && x < z) ? x : ((y < x && y < z) ? y : z);
+            int mx = (x > y && x > z) ? x : ((y > x && y > z) ? y : z);
+            int ar = x + y + z - mn - mx;
+
+            x = mn;
+            y = ar;
+            z = mx;
+
+            // Вывод переменных
+            Console.WriteLine($"x = {x} y = {y} z = {z}");
+        }
+	}
+}
