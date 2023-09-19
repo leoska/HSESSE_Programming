e3a0c9de3cf326c9b5c0c4297132f00619c8b49f
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-20 01:09:41 +0300

    Ivanchenko: [ADD] add full solution of Task04

---
+++ Seminars/Seminar02/Self/Task05/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HellowWorld
+{
+class Program
+	{
+		static void Main()
+		{
+            // Создание и считывание из консоли данных о радиусе круга и координатах точки
+            double r = 0;
+            
+            do
+            {
+                Console.Write("Введите радиус круга (R > 0): ");
+            } while (!double.TryParse(Console.ReadLine(), out r) || r <= 0);
+
+            double x = 0;
+            
+            do
+            {
+                Console.Write("Введите x-координату точки: ");
+            } while (!double.TryParse(Console.ReadLine(), out x));
+
+            double y = 0;
+            
+            do
+            {
+                Console.Write("Введите y-координату точки: ");
+            } while (!double.TryParse(Console.ReadLine(), out y));
+
+            // Вывод где лежит точка
+            Console.WriteLine($"{(x * x + y * y <= r * r ? "Точка внутри круга!" : "Точка вне круга!")}");
+        }
+	}
+}
