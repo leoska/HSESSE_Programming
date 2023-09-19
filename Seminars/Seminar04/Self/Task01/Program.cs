7df9b0b1820ad4b9b3700c455e0e71522ef1c6a0
Vilina Olkhovskaya <vaolkhovskaia@edu.hse.ru>
2023-09-19 21:50:46 +0300

    seminar04 done

---
+++ Seminars/Seminar04/Self/Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case >= '\u0030' and <= '\u0039':
+                    Console.WriteLine("a:" + a + " F(a) = 100"); break;
+                case >= '\u0041' and <= '\u005A':
+                    Console.WriteLine("a:" + a + " F(a) = 200"); break;
+                case >= '\u0061' and <= '\u007A':
+                    Console.WriteLine("a:" + a + " F(a) = 300"); break;
+                default:
+                    Console.WriteLine("a:" + a + " F(a) = 400"); break;
+
+            }
+        }
+    }
+}
