79ccff8c3c2db764e52f8baaa6251a32b6e83921
Akylbek005 <iakylbek005@gmail.com>
2023-09-20 03:34:16 +0600

    Seminar3 Ысаков Акылбек

---
+++ Seminars/Seminar03/Self/Task04/Program4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        double f;
+
+        if (x < y){
+            f = Math.Sin(x) + Math.Pow(Math.Cos(y), 2);
+        }
+        else if(x > y){
+            f = Math.Sin(Math.Pow(y, 2)) + Math.Cos(y);
+        }
+        else{
+            f = Math.Log10(x);
+        }
+        Console.WriteLine(f);
+    }
+}
