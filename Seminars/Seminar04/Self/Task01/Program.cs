1cb4f3824e2ce58837b44c08814112ae0e26055f
Rechnoi <32768298+Rechnoi@users.noreply.github.com>
2023-09-19 23:57:38 +0300

    Ivanchenko: [ADD] add full solution of Task01

---
+++ Seminars/Seminar04/Self/Task01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Prak3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Считывание символа, введенного пользователем в консоль
+            char a;
+
+            do
+            {
+                Console.Write("Введите один символ: ");
+            } while (!char.TryParse(Console.ReadLine(), out a));
+
+            // Рассчет переменной F в зависимости от введенного символа a
+            int F = a switch
+            {
+                >= '0' and <= '9' => 100,
+                >= 'A' and <= 'Z' => 200,
+                >= 'a' and <= 'z' => 300,
+                _ => 400
+            };
+
+            // Вывод введенного символа a и значения переменной F
+            Console.Write($"Символ: {a} | Ответ: {F}");
+        }
+    }
+}
