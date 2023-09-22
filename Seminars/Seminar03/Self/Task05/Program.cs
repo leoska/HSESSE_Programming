0bff372219f3b454066b59569e46f8b8520562e8
seny03 <arseniysemkin3012@gmail.com>
2023-09-22 18:09:16 +0300

    Task05 solved

---
+++ Seminars/Seminar03/Self/Task05/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task05
+{
+    class Program
+    {
+        static double ReadDouble()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            double a = ReadDouble();
+            double b = ReadDouble();
+            double c = ReadDouble();
+
+            if(a == 0)
+            {
+                Console.WriteLine($"{b}x + {c} = 0");
+                Console.WriteLine($"x = {-c / b}");
+            }
+            else
+            {
+                double D = b*b - 4*a*c;
+                if (D < 0)
+                {
+                    Console.WriteLine("Нет действительных корней");
+                }
+                else
+                {
+                    Console.WriteLine($"{a}x^2 + {b}x + {c} = 0");
+                    Console.WriteLine($"x1 = {(-b + Math.Sqrt(D)) / (2 * a)}; x2 = {(-b - Math.Sqrt(D)) / (2 * a)}");
+                }
+            }
+        }
+    }
+}
