30680a0365f0f103b8c7df4f423cfebb0d924ce6
Анна Асташкина <adastashkina@edu.hse.ru>
2023-09-26 22:56:31 +0300

    Seminar06 self programms

--- Seminars/Seminar06/Self/Self01/Program.cs
+++ Seminars/Seminar06/Self/Self01/Program.cs
@@ -1,1 +1,20 @@
-﻿// Ваше решение.
+﻿
+using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool p = false;
+            bool q = false;
+            do
+            {
+                do
+                {
+                    bool F = !(p & q) & !(p | !q);
+                    Console.WriteLine($"p={p} q={q} F={F}");
+                    q = !q;
+                } while (q);
+                p = !p;
+            } while (p);
+        }
+    }
--- Seminars/Seminar06/Self/Self03/Program.cs
+++ Seminars/Seminar06/Self/Self03/Program.cs
@@ -1,1 +1,19 @@
-﻿// Ваше решение.
+﻿using System;
+internal class Program
+{
+    static void Main()
+    {
+        int q = 0;
+        for (int i = 2000; i <= 3000; i++)
+        {
+            int a = i / 1000;
+            int b = i / 100 % 10;
+            int c = i / 10 % 10;
+            int d = i % 10;
+            if (a!=b && a!=c && a!=d && b!=c && b!=d && c!=d)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
