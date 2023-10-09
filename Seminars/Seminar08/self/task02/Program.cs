b78f92d4e32a873731b62d49aff9c9f7797a9dd9
Анна Асташкина <adastashkina@edu.hse.ru>
2023-10-09 23:13:50 +0300

    Seminar08 selfs

---
+++ Seminars/Seminar08/self/task02/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace task02
+{
+    internal class Program
+    {
+        public static bool Triangle(double x, double y, double z, out double p, out double s)
+        {
+            if (!(x + y > z && x + z > y && z + y > x) || x<=0 || y<=0 || z<=0)
+            {
+                p = 0;
+                s = 0;
+                return false;
+            }
+            else
+            {
+                p = x + y + z;
+                double p1 = p / 2;
+                s = Math.Sqrt(p1 * (p1 - x) * (p1 - y) * (p1 - x));
+                return true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите x, y, z");
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                double z = double.Parse(Console.ReadLine());
+                bool f=Triangle(x, y, z, out double p, out double s);
+                if (f)
+                {
+                    Console.WriteLine($"периментр = {p}, площадь = {s}");
+                }
+                else
+                {
+                    Console.WriteLine("Введены неподходящие данные");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введены некорректные данные");
+            }
+        }
+    }
+}
---
+++ Seminars/Seminar08/self/task04_03/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace task04_03
+{
+    internal class Program
+    {
+        static void MinMax(ref double x, ref double y)
+        {
+            double z = x;
+            x = Math.Max(x, y);
+            y = Math.Min(z, y);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите A, B, C, D");
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                double d = double.Parse(Console.ReadLine());
+
+
+                MinMax(ref a, ref b);
+                MinMax(ref c, ref d);
+                MinMax(ref b, ref d);
+                MinMax(ref a, ref c);
+                Console.WriteLine($"Максимальное число - {a}, минимальное - {d}");
+
+
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введены некорректные данные");
+            }
+        }
+    }
+}
