e4acf38e85e8c51f00f4621e3be0fa5a832971f5
Moge Lolya <mogemoge9111@gmail.com>
2023-10-10 19:59:40 +0300

    Seminar07 Task01-03

---
+++ Seminars/Seminar07/Self/Task01/Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+        static void Triangle(int a)
+        {
+            string s = "";
+            for (int i = 0; i < a; i++)
+            {
+                s += "*";
+                Console.WriteLine(s);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Triangle(n);
+        }
+    }
+}
